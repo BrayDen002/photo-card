0b12cf160b0855e2d6e655e4274b5e78ece010ba
bdalit <70769843+bdalit@users.noreply.github.com>
2023-03-22 18:05:32 -0700

    Trying to implement zoom in effect on photo cards

---
+++ style.js
@@ -0,0 +1,10 @@
+function getPics() {} //just for this demo
+const imgs = document.querySelectorAll('.gallery img');
+const fullPage = document.querySelector('.big-card');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
