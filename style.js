e53504aaa50cf5c006f25eb8dca7c84fb71317c8
bdalit <70769843+bdalit@users.noreply.github.com>
2023-03-24 15:32:15 -0700

    update

--- style.js
+++ style.js
@@ -1,10 +1,14 @@
-function getPics() {} //just for this demo
-const imgs = document.querySelectorAll('.gallery img');
-const fullPage = document.querySelector('.big-card');
+// function getPics() {} //just for this demo
+// const imgs = document.querySelectorAll('.gallery img');
+// const fullPage = document.querySelector('.big-card');
 
-imgs.forEach(img => {
-  img.addEventListener('click', function() {
-    fullPage.style.backgroundImage = 'url(' + img.src + ')';
-    fullPage.style.display = 'block';
-  });
-});
+// imgs.forEach(img => {
+//   img.addEventListener('click', function() {
+//     fullPage.style.backgroundImage = 'url(' + img.src + ')';
+//     fullPage.style.display = 'block';
+//   });
+// });
+
+function change(element) {
+        element.classList.toggle("fullsize");
+}
